a88f11cf0388f174609214107beb34449837fdfd
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-12-11 23:25:44 +0300

    IService Created

---
+++ AuthServer.Core/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IService<TEntity,TDto> where TEntity : class where TDto: class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IQueryable<TDto>>> GetAllAsync();
+        Task<Response<IEnumerable<TEntity>>> GetAllWithoutQueryAsync();
+        IQueryable<TEntity> WhereWithQuery(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
