601bf961bc3ba3197e7e04be46fc348e5e0d1d0d
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-12-11 20:20:40 +0300

    ErrorDto deleted.

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; set; }
+        public bool IsShow { get; set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors = new List<string>{error};
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;    
+        }
+    }
+}
