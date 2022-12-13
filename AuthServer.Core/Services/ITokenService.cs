a02eddd960451a625fe6b629ba2d74e4967c60ad
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-12-13 23:51:10 +0300

    ITokenService created.

---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
