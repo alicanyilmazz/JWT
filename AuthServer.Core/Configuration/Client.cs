7b9780cd1d9e28c841adeded2f8dd51fae8be2cc
AlicanYilmaz <alicanyilmaz101@gmail.com>
2022-12-13 23:51:00 +0300

    Client class created.

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+    }
+}
