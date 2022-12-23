c7551235518faeb8f553377673821ca1a576caea
alican yılmaz <alicanyilmaz101@gmail.com>
2022-12-23 23:52:43 +0300

    IUserService modified.

--- AuthServer.Core/Services/IUserService.cs
+++ AuthServer.Core/Services/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharedLibrary.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace AuthServer.Core.Services
 {
     public interface IUserService
     {
-        
+        Task<Response<>>
     }
 }
