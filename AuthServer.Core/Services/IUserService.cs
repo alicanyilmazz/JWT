b64c561b04050bcb06f1abe944cfa88363a85059
alican yılmaz <alicanyilmaz101@gmail.com>
2022-12-24 23:48:11 +0300

    CreateUserAsync,GetUserByNameAsync added.

--- AuthServer.Core/Services/IUserService.cs
+++ AuthServer.Core/Services/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using SharedLibrary.Dtos;
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace AuthServer.Core.Services
 {
     public interface IUserService
     {
-        Task<Response<>>
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string UserName);
     }
 }
