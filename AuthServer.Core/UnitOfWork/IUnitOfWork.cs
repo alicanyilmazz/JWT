eb92e103f7a4f44c95bdef65e948d13059d96e55
alican yılmaz <alicanyilmaz101@gmail.com>
2022-12-25 12:25:22 +0300

    IUnitOfWork method names changed.

--- AuthServer.Core/UnitOfWork/IUnitOfWork.cs
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -8,7 +8,7 @@ namespace AuthServer.Core.UnitOfWork
 {
     public interface IUnitOfWork
     {
-        Task SaveChangesAsync();
-        void SaveChanges();
+        Task CommitAsync();
+        void Commit();
     }
 }
