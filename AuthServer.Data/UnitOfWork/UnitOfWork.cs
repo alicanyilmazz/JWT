cb57e91e1d5441cb563b638a463815bb00567e25
alican yılmaz <alicanyilmaz101@gmail.com>
2022-12-25 12:26:39 +0300

    UnitOfWork class created and IUnitOfWork implemented.

---
+++ AuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using AuthServer.Core.UnitOfWork;
+using AuthServer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
