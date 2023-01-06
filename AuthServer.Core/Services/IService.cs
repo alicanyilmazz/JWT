1aef949ccaa8f2fd8c7c960fc0a3504246becc8f
alican yılmaz <alicanyilmaz101@gmail.com>
2023-01-07 02:31:43 +0300

    IService return types changed.

--- AuthServer.Core/Services/IService.cs
+++ AuthServer.Core/Services/IService.cs
@@ -11,12 +11,12 @@ namespace AuthServer.Core.Services
     public interface IService<TEntity,TDto> where TEntity : class where TDto: class
     {
         Task<Response<TDto>> GetByIdAsync(int id);
-        Task<Response<IQueryable<TDto>>> GetAllWithAsync();
+        Task<Response<IEnumerable<TDto>>> GetAllWithAsync();
         Task<Response<IEnumerable<TDto>>> GetAllAsync();
-        Task<Response<IQueryable<TDto>>> WhereWith(Expression<Func<TEntity, bool>> predicate);
+        Task<Response<IEnumerable<TDto>>> WhereWith(Expression<Func<TEntity, bool>> predicate);
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> predicate);
-        Task<Response<TDto>> AddAsync(TEntity entity);
-        Task<Response<NoDataDto>> Remove(TEntity entity);
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto entity);
+        Task<Response<NoDataDto>> Remove(int id);
+        Task<Response<NoDataDto>> Update(TDto entity, int id);
     }
 }
