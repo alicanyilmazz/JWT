80cacd96e7f50a19c4a3d30ac2b9f76b55e174f6
alican yılmaz <alicanyilmaz101@gmail.com>
2023-02-26 22:23:40 +0300

    Authentication Service created.

---
+++ AuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,83 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly List<Client> _clients;
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<UserApp> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<UserRefreshToken> _userRefreshTokenRepository;
+
+        public AuthenticationService(IOptions<List<Client>> optionsClient, ITokenService tokenService, UserManager<UserApp> userManager, IUnitOfWork unitOfWork, IRepository<UserRefreshToken> userRefreshTokenRepository)
+        {
+            _clients = optionsClient.Value;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+            _userRefreshTokenRepository = userRefreshTokenRepository;
+        }
+
+        public async Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            if (loginDto == null)
+            {
+                throw new ArgumentNullException(nameof(loginDto));
+            }
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+            {
+                return Response<TokenDto>.Fail("Email or Password is wrong.", 400, true);
+            }
+
+            if (!await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Response<TokenDto>.Fail("Email or Password is wrong.", 400, true);
+            }
+            var token = _tokenService.CreateToken(user);
+
+            var userRefreshToken = await _userRefreshTokenRepository.Where(x => x.UserId == user.Id).SingleOrDefaultAsync(); // We check if there is a refresh token before
+            if (userRefreshToken == null)
+            {
+                await _userRefreshTokenRepository.AddAsync(new UserRefreshToken { UserId = user.Id, Code = token.RefreshToken, Expiration = token.RefreshTokenExpration });
+            }
+            else
+            {
+                userRefreshToken.Code = token.RefreshToken;
+                userRefreshToken.Expiration= token.RefreshTokenExpration;
+            }
+            await _unitOfWork.CommitAsync();
+            return Response<TokenDto>.Success(token, 200);
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClientAsync(ClientTokenDto clientTokenDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenByRefreshTokenAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshTokenAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
