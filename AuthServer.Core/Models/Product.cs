4e4b9e193290fa8be08d6a6a0002d1662b39408b
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-20 19:05:46 +0300

    UserId type changed in Product.

--- AuthServer.Core/Models/Product.cs
+++ AuthServer.Core/Models/Product.cs
@@ -12,6 +12,6 @@ namespace AuthServer.Core.Models
         public string Name { get; set; }
         public Decimal Price { get; set; }
         public int Stock { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
     }
 }
