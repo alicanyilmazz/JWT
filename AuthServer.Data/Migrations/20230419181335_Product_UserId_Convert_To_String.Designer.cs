d82aaa6b348e6f2d715d6233b46b71e4d2788c57
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-20 19:06:16 +0300

    Created new migration UserId type changes.

--- AuthServer.Data/Migrations/20230419181335_Product_UserId_Convert_To_String.Designer.cs
+++ AuthServer.Data/Migrations/20230419181335_Product_UserId_Convert_To_String.Designer.cs
@@ -4,6 +4,7 @@ using AuthServer.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthServer.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230419181335_Product_UserId_Convert_To_String")]
+    partial class Product_UserId_Convert_To_String
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
