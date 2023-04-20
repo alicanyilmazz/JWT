b89c56db7b233fcf407dcc1dc3c14d71aa8a7fc9
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-20 19:05:25 +0300

    UserId type changed in ProductDto.

--- AuthServer.Core/Dtos/ProductDto.cs
+++ AuthServer.Core/Dtos/ProductDto.cs
@@ -11,6 +11,6 @@ namespace AuthServer.Core.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public Decimal Price { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
     }
 }
