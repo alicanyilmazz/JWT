d382e6584523980143b398b57939ced7e5925523
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-22 00:52:47 +0300

    New API added.

---
+++ MiniApp4.API/Program.cs
@@ -0,0 +1,30 @@
+using SharedLibrary.Configuration;
+using SharedLibrary.Extensions.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOptions"));
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOption>();
+builder.AddCustomTokenAuth(tokenOptions);
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
