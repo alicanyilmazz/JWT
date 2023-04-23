65a9a984306fef0bbae387122f00e9a792f65ba7
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-24 01:02:43 +0300

    CustomControllerBase created.

---
+++ MiniApp4.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace MiniApp4.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(Response<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
