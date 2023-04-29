df2ba247f0ae898b740f9cc4f3549c64894f7ee9
alican yılmaz <alicanyilmaz101@gmail.com>
2023-04-30 01:18:46 +0300

    ImageInputModel created.

---
+++ MiniApp4.API/Utilities/Visual/ImageInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniApp4.API.Utilities.Visual
+{
+    public class ImageInputModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Stream Content { get; set; }
+    }
+}
