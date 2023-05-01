16cf78a902bd097c19ff0acbe0fada09f01d9082
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-02 02:24:56 +0300

    IImageManager created.

---
+++ MiniApp4.API/Utilities/Visual/Abstract/IImageManager.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniApp4.API.Utilities.Visual.Abstract
+{
+    public interface IImageManager
+    {
+        public Task Process(IImageServices services, IEnumerable<ImageInputModel> images);
+    }
+}
