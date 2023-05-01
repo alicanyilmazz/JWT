559b2e286c78941e8388e4e2725ce0debcc1816d
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-02 02:25:32 +0300

    ImageManager created.

---
+++ MiniApp4.API/Utilities/Visual/Concrete/ImageManager.cs
@@ -0,0 +1,12 @@
+﻿using MiniApp4.API.Utilities.Visual.Abstract;
+
+namespace MiniApp4.API.Utilities.Visual.Concrete
+{
+    public class ImageManager : IImageManager
+    {
+        public async Task Process(IImageServices services, IEnumerable<ImageInputModel> images)
+        {
+            await services.ProcessAsync(images);
+        }
+    }
+}
