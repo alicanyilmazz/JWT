99873035e9aec0c6a58cd3a6b26406ff1e9d3d9f
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-06 23:44:06 +0300

    New controller created.

---
+++ MiniApp4.API/Controllers/PhotographController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniApp4.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotographController : ControllerBase
+    {
+    }
+}
