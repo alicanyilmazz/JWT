06c174bd77c122e5fb4e502b7247605eb5fa06a1
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-14 13:20:40 +0300

    CustomBaseController created.

---
+++ MiniApp3.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace MiniApp3.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(Response<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+
+        public IActionResult ActionResultFileInstance<T>(Response<T> response) where T : class
+        {
+            return File();
+        }
+    }
+}
