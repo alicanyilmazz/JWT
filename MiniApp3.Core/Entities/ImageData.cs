e1852b1448e6822791725077b0299cede4e58594
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-14 13:22:17 +0300

    ImageData created.

---
+++ MiniApp3.Core/Entities/ImageData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp3.Core.Entities
+{
+    public class ImageData
+    {
+        public ImageData() 
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string OriginalType { get; set; }
+        public string  OriginalFileName { get; set; }
+        public byte[] OriginalContent { get; set; }
+        public byte[] ThumbnailContent { get; set; }
+        public byte[] FullScreenContent { get; set; }
+
+    }
+}
