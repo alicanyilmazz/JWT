fb2f3416374c74e0818f6f25eb22d30c62c72a7b
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-14 13:20:27 +0300

    Magnitude created.

---
+++ MiniApp3.API/Common/Constants/Magnitude.cs
@@ -0,0 +1,8 @@
+﻿namespace MiniApp3.API.Common.Constants
+{
+    public class Magnitude
+    {
+        public const int ThreeMegabytes = 3 * 1024 * 1024;
+        public const int FourMegabytes = 4 * 1024 * 1024;
+    }
+}
