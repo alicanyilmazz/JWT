a7fd677ced217bd4e72f4a24ae24f135ef8eed98
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-19 17:31:49 +0300

    Models created.

---
+++ MiniApp3.Core/Entities/ImageInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp3.Core.Entities
+{
+    public class ImageInformation
+    {
+        public string Id { get; set; }
+    }
+}
