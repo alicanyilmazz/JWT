2209e989a9c13a17b24803ad7d9db498810dc169
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-28 23:52:05 +0300

    IImageQualityRepository created.

---
+++ MiniApp3.Core/Repositories/IImageQualityRepository.cs
@@ -0,0 +1,15 @@
+﻿using MiniApp3.Core.Dtos.StoredProcedureDto;
+using MiniApp3.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp3.Core.Repositories
+{
+    public interface IImageQualityRepository
+    {
+        public Task<List<ImageQualityResult>> GetImageQualityConfigs();
+    }
+}
