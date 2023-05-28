b3f7a69a3373243ceafa0630f04c56fa4b0e015d
alican yılmaz <alicanyilmaz101@gmail.com>
2023-05-28 23:51:00 +0300

    ImageQualityResult added.

---
+++ MiniApp3.Core/Dtos/StoredProcedureDto/ImageQualityResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniApp3.Core.Dtos.StoredProcedureDto
+{
+    public class ImageQualityResult
+    {
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public int ResizeWidth { get; set; }
+        public bool IsOriginal { get; set; }
+    }
+}
