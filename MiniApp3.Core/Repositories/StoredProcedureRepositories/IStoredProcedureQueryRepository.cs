4fc9997863976b6d04741c166acefe147dfb0f01
alican yılmaz <alicanyilmaz101@gmail.com>
2023-07-10 14:02:51 +0300

    Methods refactored.

--- MiniApp3.Core/Repositories/StoredProcedureRepositories/IStoredProcedureQueryRepository.cs
+++ MiniApp3.Core/Repositories/StoredProcedureRepositories/IStoredProcedureQueryRepository.cs
@@ -9,9 +9,10 @@ namespace MiniApp3.Core.Repositories.StoredProcedureRepositories
 {
     public interface IStoredProcedureQueryRepository
     {
-        public Task<List<ServerImagesInformation>> GetImages(string imageId);
+        public Task<List<ServerImagesInformation>> GetImage(string imageId);
+        public Task<List<ServerImagesInformation>> GetImages();
         public Task<List<ImageQualityResponse>> GetImageQualityConfigs();
-
+        public Task<Stream> ReadPhotoDirectlyFromDatabase(string id, string content);
         /// ExecuteSqlInterpolatedAsync yöntemi, çıktı parametrelerini doğrudan desteklemez. Bu nedenle, çıktı parametresi kullanırken ExecuteSqlRawAsync yöntemini kullanmanız gerekmektedir.
         /// <summary>
         /// This methods returns number of ImageFile record.
