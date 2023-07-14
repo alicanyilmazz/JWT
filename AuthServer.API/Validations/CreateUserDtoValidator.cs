2e2d8a8d7877a48e5bc8706b3932673a1b14f4ed
alican yılmaz <alicanyilmaz101@gmail.com>
2023-07-14 10:09:23 +0300

    CreateUserDtoValidator created.

---
+++ AuthServer.API/Validations/CreateUserDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using AuthServer.Core.Dtos;
+using FluentValidation;
+
+namespace AuthServer.API.Validations
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        List<string> blacklistedWords = new List<string>() { "Alican", "yilmaz" };
+        public CreateUserDtoValidator()
+        {
+            RuleFor(x=>x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.UserName).NotEmpty();
+
+            RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .Matches("[A-Z]").WithMessage("'{PropertyName}' must contain one or more capital letters.")
+            .Matches("[a-z]").WithMessage("'{PropertyName}' must contain one or more lowercase letters.")
+            .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits.")
+            .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("'{ PropertyName}' must contain one or more special characters.")
+            .Matches("^[^£# “”]*$").WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.")
+            .Must(pass => !blacklistedWords.Any(word => pass.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0))
+                .WithMessage("'{PropertyName}' contains a word that is not allowed.");
+        }
+    }
+}
